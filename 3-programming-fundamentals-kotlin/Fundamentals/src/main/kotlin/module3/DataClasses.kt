264e45c63c392934979a20cc96c871e6bf31a6d8
Mehmet Tarhan <hi@memtarhan.com>
2024-12-03 22:05:56 +0300

    [Kotlin] - data classes

--- 3-programming-fundamentals-kotlin/Fundamentals/src/main/kotlin/module3/DataClasses.kt
+++ 3-programming-fundamentals-kotlin/Fundamentals/src/main/kotlin/module3/DataClasses.kt
@@ -5,6 +5,9 @@ data class Dog3(
     val age: Int
 )
 
+class ValueClass(val value: Int)
+data class ValueDataClass(val value: Int)
+
 fun main() {
     val dog = Dog3("Pluto", 7)
     val (name, age) = dog
@@ -15,4 +18,13 @@ fun main() {
     at correct positions. For instance, if you place age at the position of name, and name at the position of age,
     then you will have age in a variable called name, and name in the variable called age.
      */
+
+
+    val value1 = ValueClass(42)
+    val value2 = ValueClass(42)
+    println(value1 == value2) // False
+
+    val value3 = ValueDataClass(42)
+    val value4 = ValueDataClass(42)
+    println(value3 == value4) // True
 }
