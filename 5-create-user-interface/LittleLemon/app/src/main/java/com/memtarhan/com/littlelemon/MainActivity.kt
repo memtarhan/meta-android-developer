70c6dfec49ca429fe9e057e5a3e858fe62aa6259
Mehmet Tarhan <hi@memtarhan.com>
2025-10-02 13:37:47 +0100

    <UI>: Exercise: Building a responsive layout

--- 5-create-user-interface/LittleLemon/app/src/main/java/com/memtarhan/com/littlelemon/MainActivity.kt
+++ 5-create-user-interface/LittleLemon/app/src/main/java/com/memtarhan/com/littlelemon/MainActivity.kt
@@ -1,46 +1,128 @@
 package com.memtarhan.com.littlelemon
 
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import com.memtarhan.com.littlelemon.ui.theme.Pink80
+import com.memtarhan.com.littlelemon.ui.theme.Purple80
+import com.memtarhan.com.littlelemon.ui.theme.PurpleGrey80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            val materialBlue700 = Color(0xFF1976D2)
+            val scaffoldState = rememberDrawerState(DrawerValue.Closed)
+            Scaffold(
+                topBar = {
+                    TopAppBar()
+                },
+                content = { paddingValues ->
+                    MenuContent(paddingValues = paddingValues)
+                }
+            )
         }
     }
 }
 
-
 @Composable
-fun HomeScreen() {
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            ModalDrawerSheet { /* Drawer content */ }
-        },
-    ) {
-        Scaffold(
-            topBar = {
-                TopAppBar()
+fun MenuContent(paddingValues: PaddingValues) {
+    Surface(modifier = Modifier.padding(paddingValues)) {
+        val menuPadding = 8.dp
+        val configuration = LocalConfiguration.current
+        when (configuration.orientation) {
+            ORIENTATION_LANDSCAPE -> {
+                Column {
+                    Row(modifier = Modifier.weight(0.5f)) {
+                        Text(
+                            "Appetizers",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .background(Purple80)
+                                .padding(menuPadding)
+                        )
+                        Text(
+                            "Salads",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .padding(menuPadding)
+                        )
+                    }
+                    Row(modifier = Modifier.weight(0.5f)) {
+                        Text(
+                            "Drinks",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .background(Pink80)
+                                .padding(menuPadding)
+                        )
+                        Text(
+                            "Mains",
+                            modifier = Modifier
+                                .weight(0.25f)
+                                .fillMaxHeight()
+                                .background(PurpleGrey80)
+                                .padding(menuPadding)
+                        )
+                    }
+                }
             }
-        ) { contentPadding ->
-            Column {
-                UpperPanel()
-                LowerPanel()
+
+            else -> {
+                Column {
+                    Text(
+                        "Appetizers",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .background(Purple80)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                    Text(
+                        "Salads",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                    Text(
+                        "Drinks",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .background(Pink80)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                    Text(
+                        "Mains",
+                        modifier = Modifier
+                            .weight(0.25f)
+                            .background(PurpleGrey80)
+                            .padding(menuPadding)
+                            .fillMaxWidth()
+                    )
+                }
             }
         }
     }
