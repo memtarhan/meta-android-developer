684ec2d2b8a5cac052fa28593e23cecd630f58d6
Mehmet Tarhan <hi@memtarhan.com>
2025-10-02 07:03:17 +0100

    <UI>: Modifiers with layouts

--- 5-create-user-interface/LittleLemon/app/src/main/java/com/memtarhan/com/littlelemon/MainActivity.kt
+++ 5-create-user-interface/LittleLemon/app/src/main/java/com/memtarhan/com/littlelemon/MainActivity.kt
@@ -4,21 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -44,7 +47,7 @@ fun MainComponent() {
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start,
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
             .background(Color(0XFF495E57))
     ) {
         Text(
@@ -59,22 +62,36 @@ fun MainComponent() {
             fontSize = 24.sp,
             color = Color(0xFFF4CE14),
             modifier = Modifier.padding(start = 20.dp),
+        )
+
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp), horizontalArrangement = Arrangement.Start
+        ) {
+            Text(
+                text = stringResource(id = R.string.description),
+                Modifier.width(200.dp),
+                color = Color.White,
+                fontSize = 21.sp
             )
-
-        Row(Modifier.fillMaxSize(), horizontalArrangement = Arrangement.Center) {
-            Button(
-                onClick = {/* TODO */ },
-                border = BorderStroke(1.dp, Color.Red),
-                shape = RoundedCornerShape(10.dp),
-            ) {
-                Text(text = stringResource(order))
-            }
-
             Image(
                 painter = painterResource(R.drawable.restaurantfoodb),
                 contentDescription = "",
-                Modifier.height(100.dp)
+                Modifier
+                    .height(100.dp)
+                    .clip(RoundedCornerShape(20.dp))
             )
         }
+
+        Button(
+            onClick = {/* TODO */ },
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.padding(horizontal = 20.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0XFFF4CE14))
+        ) {
+            Text(text = stringResource(order))
+        }
+
     }
 }
