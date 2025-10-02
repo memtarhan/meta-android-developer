30074e21c154555fb47b10acd4297d91f1d61232
Mehmet Tarhan <hi@memtarhan.com>
2025-10-02 09:07:54 +0100

    <UI>: Declaring state

--- 5-create-user-interface/LittleLemon/app/src/main/java/com/memtarhan/com/littlelemon/MainActivity.kt
+++ 5-create-user-interface/LittleLemon/app/src/main/java/com/memtarhan/com/littlelemon/MainActivity.kt
@@ -4,15 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HomeScreen()
+            ItemOrder()
         }
     }
 }
@@ -22,5 +41,51 @@ fun HomeScreen() {
     Column {
         UpperPanel()
         LowerPanel()
+    }
+}
+
+
+@Composable
+private fun ItemOrder() {
+    var count by remember {
+        mutableIntStateOf(0)
+    }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Greek Salad",
+            fontSize = 30.sp
+        )
+
+        Row(
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = {
+                count--
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "Remove"
+                )
+            }
+
+            Text(
+                text = "$count",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(8.dp)
+            )
+
+            IconButton(onClick = {
+                count++
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add"
+                )
+            }
+        }
     }
 }
