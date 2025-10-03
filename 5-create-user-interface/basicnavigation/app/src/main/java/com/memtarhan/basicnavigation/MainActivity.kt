5fd47ca2407b5f4e5908f0b36b028dfddfee17f2
Mehmet Tarhan <hi@memtarhan.com>
2025-10-03 07:47:05 +0100

    <UI>: Navigation in Compose

---
+++ 5-create-user-interface/basicnavigation/app/src/main/java/com/memtarhan/basicnavigation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.memtarhan.basicnavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyNavigation()
+        }
+    }
+}
+
+
+@Composable
+fun MyNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Home.route) {
+        composable(Home.route) { HomeScreen(navController) }
+
+        composable(MenuList.route) { MenuListScreen() }
+    }
+}
